c77977f6c7eaa1f8a7fcb7ed323bd2105e8a0982
Daniel Della-Rose <danieldella-rose@Daniels-MacBook-Pro.local>
2020-09-18 11:28:42 -0600

    7 restful routes up and running

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,6 +3,38 @@ const Blog = require('../models/blogs.js')
 const router = express.Router()
 
 
+router.delete('/:id', (req, res) => {
+  Blog.findByIdAndRemove(req.params.id, (err, data) => {
+    res.redirect('/blogs')
+  })
+})
+
+router.get('/:id/edit', (req, res) => {
+  Blog.findById(req.params.id, (error, foundBlog) => {
+    res.render('edit.ejs', {
+      log: foundBlog
+      // ,currentUser: req.session.currentUser
+    })
+  })
+
+})
+
+router.put('/:id', (req, res) => {
+  Blog.findByIdAndUpdate(req.params.id, req.body,{
+    new:true
+  },
+    (err, updateModel) => {
+    res.redirect('/blogs')
+  })
+  // res.send(req.body)
+})
+
+router.get('/new', (req, res) => {
+  res.render('new.ejs',
+  // {currentUser: req.session.currentUser}
+  )
+})
+
 router.get('/seed', (req, res) => {
     Blog.create(
         [{
@@ -27,6 +59,18 @@ router.get('/seed', (req, res) => {
     )
 });
 
+router.get('/:id', (req, res) => {
+    Blog.findById(req.params.id, (error, foundLog) => {
+        res.render(
+            'show.ejs',
+            {
+                blog:foundLog
+                // ,currentUser: req.session.currentUser
+            }
+        );
+    })
+});
+
 
 router.get('/' , (req, res) => {
   Blog.find({}, (error, allBlogs) => {
@@ -39,6 +83,7 @@ router.get('/' , (req, res) => {
         );
     })
 });
+
 
 
 
